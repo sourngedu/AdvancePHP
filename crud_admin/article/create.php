2dfbe3ce88916e20e6b9cec498bf4bd0430b48a4
Sourng Edu <sourngedu@gmail.com>
2019-08-06 10:25:42 +0700

    updates

--- crud_admin/article/create.php
+++ crud_admin/article/create.php
@@ -1,18 +1,115 @@
+<?php
+// Initialize the session
+// session_start();
+
+date_default_timezone_set("Asia/Phnom_Penh");
+
+include_once('config.php');
+
+$title=$detail=$category_id=$create_id="";
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $title=$_POST['title'];
+    $detail=$_POST['detail'];
+    $category_id=$_POST['category'];
+    $photo="default.jpg";
+    $today = date("Y-m-d h:i:s");
+    $create_id= $_POST['username'];
+
+    $status=1;
+
+   
+
+
+    // echo $title ." - ". $detail ." - ". $category_id;
+
+
+    // Prepare an insert statement
+    $sql = "INSERT INTO article (category_id, title,detail,photo,created_by,created_date,status) 
+                        VALUES (:category_id, :title,:detail,:photo,:created_by,:created_date,:status)";
+ 
+    if($stmt = $pdo->prepare($sql)){
+        // Bind variables to the prepared statement as parameters
+        $stmt->bindParam(":category_id", $category_id);
+        $stmt->bindParam(":title", $title);
+        $stmt->bindParam(":detail", $detail);
+        $stmt->bindParam(":photo", $photo);
+        $stmt->bindParam(":created_by", $create_id);
+        $stmt->bindParam(":created_date", $today);
+        $stmt->bindParam(":status", $status);
+      
+                 
+            
+        // Attempt to execute the prepared statement
+        if($stmt->execute()){
+            // Records created successfully. Redirect to landing page
+            header("location: ../home.php?page=article&frm=index");
+            exit();
+            // echo "Hello Success !";
+        } else{
+            echo "Something went wrong. Please try again later.";
+        }
+    }
+         
+    // Close statement
+    // unset($stmt);
+
+
+
+
+}
+
+?>
+
+
 <h3>Create Article</h3>
-<form action="/action_page.php" class="needs-validation" novalidate>
+
+<form action="article/create.php" method="post" class="needs-validation" novalidate>
+
+    <input type="hidden" value="<?php echo $_SESSION["username"]; ?>" name="username">
     <div class="form-group">
-        <label for="cname">Category Name:</label>
-        <input type="text" class="form-control" id="cname" placeholder="Enter Category Name" name="cname" required>
+        <label for="cname">Title:</label>
+        <input type="text" class="form-control" id="title" placeholder="Enter Title" name="title" required>
         <div class="valid-feedback">Valid.</div>
         <div class="invalid-feedback">Please fill out this field.</div>
     </div>
+
     <div class="form-group">
-        <label for="cicon">Category Icon:</label>
-        <input type="text" class="form-control" id="cicon" placeholder="Enter fa fa-users" name="cicon" required>
-        <div class="valid-feedback">Valid.</div>
-        <div class="invalid-feedback">Please fill out this field.</div>
+    <label for="comment">Detail:</label>
+    <textarea class="form-control" rows="5" id="detail" name="detail"></textarea>
     </div>
-    
+
+    <div class="form-group">
+    <label for="sel1">Category:</label>
+    <?php
+        // Attempt select query execution
+        $sql = "SELECT * FROM category";
+        $result = $pdo->query($sql);
+        if($result){
+        if($result->rowCount() > 0){
+    ?>
+    <select class="form-control" id="sel1" name="category">
+        <?php
+            while($row = $result->fetch()){
+            ?>    
+                <option value="<?php echo $row['category_id']; ?>"><?php  echo $row['name']; ?></option>  
+
+                <?php
+                }
+            }
+        }
+        
+                            // Close connection
+                            // unset($pdo);
+        ?>
+
+       
+   
+    </select>
+    </div>
+
+   
     
     <button type="submit" class="btn btn-primary">Create</button>
 </form>
--- crud_admin/article/index.php
+++ crud_admin/article/index.php
@@ -1,3 +1,4 @@
+
 <div>
 <h4 class="m-0 font-weight-bold text-primary float-left">Article List</h4>
 <!-- <a  class="float-right"href="">Add New</a> -->
@@ -8,31 +9,67 @@
     <span class="text">Add New Article</span>
 </a>
 </div>
+<div class="table-responsive" style="margin-top: 50px;">  
+    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" >
+    <!-- <table class="table table-bordered" style="margin-top: 50px;"> -->
+    <?php
+        // Attempt select query execution
+        $sql = "SELECT * FROM article";
+        $result = $pdo->query($sql);
+        if($result){
+        if($result->rowCount() > 0){
+        ?>
 
+        <thead>
+            <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Create Date</th>
+                    
+            <th>Action</th>
+            </tr>
+        </thead>
 
-<table class="table table-bordered" style="margin-top: 50px;">
-    <thead>
-    <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-    </tr>
-    <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-    </tr>
-    <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-    </tr>
-    </tbody>
-</table>
+        <tfoot>
+            <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Create Date</th>
+                    
+            <th>Action</th>
+            </tr>
+        </tfoot>
+
+        <tbody>
+            <?php
+            while($row = $result->fetch()){
+            ?>    
+                <tr class="success">
+                    <td><?php echo $row['category_id']?></td>
+                    <td><a href="read.php?id=<?php echo $row['id']; ?>" title='Detail <?php echo $row['title']?>' data-toggle='tooltip' ><?php echo $row['title']?></a></td>
+                    <td><?php echo $row['created_date']?></td>
+                                
+                    <td>
+                        <a href="read.php?id=<?php echo $row['id']; ?>" class="btn btn-info btn-sm">
+                            <i class="fas fa-eye"></i> 
+                        </a>
+                        
+                        <a href="update.php?id=<?php echo $row['id']; ?>" class="btn btn-success btn-sm">
+                            <i class="fas fa-check"></i> 
+                        </a>
+                        <a class='btn btn-danger btn-sm' href="delete.php?id=<?php echo $row['id']; ?>" title='Delete Record' data-toggle='tooltip'><i class="fas fa-trash"></i> </a>
+                    </td>
+
+                </tr>
+        
+                <?php
+            }
+            }
+        }
+        
+                            // Close connection
+                            // unset($pdo);
+            ?>
+            </tbody>
+    </table>
+</div>
